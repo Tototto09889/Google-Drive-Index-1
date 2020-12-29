1d2e4867525c819bc2bfcd016bc1a17b1be71dc1
kuro-creator <74273512+kuro-creator@users.noreply.github.com>
2020-12-29 18:22:48 +0700

    Update and rename app without credit dan copyright.js to app without credit and copyright.js

--- js/app without credit and copyright.js
+++ js/app without credit and copyright.js
@@ -73,9 +73,9 @@ function title(path) {
 	// $('title').html(document.siteName + ' - ' + path);
 	var model = window.MODEL;
 	if (model.is_search_page)
-		(`$('title') - ${drive_name} - Search results for ${model.q} `);
+		(`$('title').html - ${'drive_name} - Search results for ${model.q} `);
 	else
-		(`$('title') - ${drive_name} - ${path}`);
+		(`$('title').html - ${'drive_name} - ${path}`);
 }
 
 // Render the navigation bar
