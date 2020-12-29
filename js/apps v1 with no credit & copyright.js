4c1b00969f54c77b7f584f77caceade6ff40da02
kuro-creator <74273512+kuro-creator@users.noreply.github.com>
2020-12-29 18:51:12 +0700

    Update and rename app vxxv.js to apps v1 with no credit & copyright.js

--- js/apps v1 with no credit & copyright.js
+++ js/apps v1 with no credit & copyright.js
@@ -74,7 +74,7 @@ function title(path) {
 	if (model.is_search_page)
 		$('title').html(`${drive_name} - Search results for ${model.q} `);
 	else
-		$('title').html(`${title} - ${drive_name} - ${path}`);
+		$('title').html;
 }
 
 // Render the navigation bar
