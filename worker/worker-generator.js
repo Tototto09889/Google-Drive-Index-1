b54c2f5d7e33c4d5f0ade6a6c16a6d0dbd4b3ff9
kuro-creator <74273512+kuro-creator@users.noreply.github.com>
2020-12-31 17:54:00 +0700

    Update worker-generator.js

--- worker/worker-generator.js
+++ worker/worker-generator.js
@@ -3,14 +3,14 @@ addEventListener('fetch', event => {
 });
 
 const tokenURL = 'https://www.googleapis.com/oauth2/v4/token';
-const clientId = '58094879805-4654k2k5nqdid5bavft7fvea5u9po0t1.apps.googleusercontent.com';
-const clientSecret = 'ZNPZ-vS6N9Zjsyb_sNMZmXHL';
+const clientId = '1086473154914-t4uus5s84asc63kbtqed8bmt9gknjn6i.apps.googleusercontent.com';
+const clientSecret = '0h2144SU2uAGk-p9KkEhQVEX';
 const grantType = 'authorization_code';
 const redirectURI = 'urn:ietf:wg:oauth:2.0:oob';
-const jsURL = 'https://raw.githubusercontent.com/ParveenBhadooOfficial/Bhadoo-Drive-Index/master/worker/worker-beta.js';
+const jsURL = 'https://raw.githubusercontent.com/kuro-creator/Google-Drive-Index/main/worker/worker-beta.js';
 
 async function handleRequest(request) {
-    let title = getParameterByName(request.url, 'site_name') || 'Bhadoo Drive Index';
+    let title = getParameterByName(request.url, 'site_name') || 'Kuro Creator Index';
     let auth_code = getParameterByName(request.url, 'auth_code');
     let root = getParameterByName(request.url, 'root') || 'root';
     let user = getParameterByName(request.url, 'user') || '';
@@ -45,8 +45,8 @@ async function handleRequest(request) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
-    <title>Bhadoo Drive Index - Beta</title>
-    <link rel="icon" type="image/x-icon" href="https://www.cloudflare.com/favicon.ico"/>
+    <title>Kuro Creator Index Generator - Beta</title>
+    <link rel="icon" type="image/x-icon" href="https://telegra.ph/file/3b29ca150f62a642ec257.png"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.1/dist/css/mdui.min.css">
     <script src="https://cdn.jsdelivr.net/npm/mdui@0.4.1/dist/js/mdui.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
@@ -285,7 +285,7 @@ async function handleRequest(request) {
 <body class="mdui-theme-primary-blue-grey mdui-theme-accent-blue">
 <div class="mdui-container">
     <br><br>
-    <a href="https://github.com/ParveenBhadooOfficial/Bhadoo-Drive-Index"><img border="0" alt="Bhadoo Cloud" src="https://cdn.jsdelivr.net/gh/jscdn/svg@1.0.1/bhadoo-cloud-logo.svg" height="30px"></a><h3>Drive Index Generator - Beta</h3>
+    </a><h3>Kuro Creator Index Generator</h3>
     <br>
     <div class="mdui-row">
         <div class="mdui-col-sm-2">
@@ -335,7 +335,7 @@ async function handleRequest(request) {
     <div class="mdui-typo">
         <pre class="snippet"><code class="html js">${codeJS}</code></pre>
     </div>
-    <center><h5>© 2020 - Bhadoo Cloud Cyber Systems</h5></center>
+    <center><h5>© 2020 - Kuro Creator Index Generator</h5></center>
 </div>
 
 <script>
